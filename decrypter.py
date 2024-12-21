2c92dc5ec13d36e759fba8f0d172cc9ca95ec628
@jhonymasetto <jhony@unochapeco.edu.br>
2024-12-21 16:28:55 -0300

    correções

--- decrypter.py
+++ decrypter.py
@@ -15,7 +15,7 @@ decrypt_data = aes.decrypt(file_data)
 ## remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
+## criar o arquivo descriptografado 
 new_file = "myfile.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
